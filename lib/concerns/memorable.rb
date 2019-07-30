276f6cb760c8985a46ad6c8f3412ede871c638b0
Robinson Regalado <rregalado17@gmail.com>
2019-07-30 02:49:27 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,5 +1,16 @@
 module Memorable
 
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+ 
+    def count
+      self.all.count
+    end
+  end
+  
+  
   def reset_all #remove the self since self would be the module in this case
     self.all.clear
   end
