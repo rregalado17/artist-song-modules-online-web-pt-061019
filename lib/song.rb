5ab1a45cbcec240ec0ebcf9600ad0f00d8bbdcdc
Robinson Regalado <rregalado17@gmail.com>
2019-07-30 03:00:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,9 @@ require 'pry'
 class Song
   
   extend Memorable::ClassMethods
-  extend Findable
-  include Paramable 
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
