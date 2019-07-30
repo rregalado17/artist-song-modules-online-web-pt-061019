afe0ba4e86fdc0030f1575717e457f966cef6e62
Robinson Regalado <rregalado17@gmail.com>
2019-07-30 01:54:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,8 @@ require 'pry'
 class Song
   attr_accessor :name
   attr_reader :artist
+  
+  include 
 
   @@songs = []
 
@@ -18,13 +20,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+#  def self.reset_all
+#    self.all.clear
+#  end
 
-  def self.count
-    self.all.count
-  end
+#  def self.count
+#    self.all.count
+#  end
 
   def artist=(artist)
     @artist = artist
